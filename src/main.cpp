1c85b0149472295d98e093c63464989ab66364e4
CW-B-W <cw.brad.wang@gmail.com>
2023-08-01 21:15:31 +0800

    Successfully Tx/Rx frames by both Master and Slave

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,16 @@ extern "C" {
 SoftwareLin swLin(RX_PIN, TX_PIN);
 extern l_bool open_lin_hw_break_reg;
 
-// #define LIN_MASTER
-#define LIN_SLAVE
+#define LIN_MASTER
+// #define LIN_SLAVE
 
 #ifdef LIN_MASTER
 volatile t_open_lin_master_state master_state_prev = OPEN_LIN_MASTER_IDLE;
 volatile t_open_lin_master_state master_state = OPEN_LIN_MASTER_IDLE;
 void open_lin_master_state_callback(t_open_lin_master_state new_state)
 {
+    // printf("master_state = %d\n", new_state);
+
     master_state_prev = master_state;
     master_state = new_state;
     if (new_state == OPEN_LIN_MASTER_IDLE) {
@@ -37,13 +39,15 @@ volatile t_open_lin_slave_state slave_state_prev = OPEN_LIN_SLAVE_IDLE;
 volatile t_open_lin_slave_state slave_state = OPEN_LIN_SLAVE_IDLE;
 void open_lin_slave_state_callback(t_open_lin_slave_state new_state)
 {
+    // printf("slave_state = %d\n", new_state);
+
     slave_state_prev = slave_state;
     slave_state = new_state;
     if (new_state == OPEN_LIN_SLAVE_IDLE) {
         swLin.endFrame();
     }
     
-    if (slave_state != slave_state_prev && slave_state == OPEN_LIN_MASTER_IDLE) {
+    if (slave_state != slave_state_prev && slave_state == OPEN_LIN_SLAVE_IDLE) {
         // delay(1000);
     }
 }
@@ -54,19 +58,39 @@ extern "C" void app_main()
     Serial.begin(115200);
     swLin.begin(9600);
 
-    uint8_t master_data_buffer_tx[] = {'h', 'e', 'l', 'l', 'o'};
-    t_master_frame_table_item master_frame_table[1] = {
-            {10,0,{0x02,OPEN_LIN_FRAME_TYPE_TRANSMIT,sizeof(master_data_buffer_tx), master_data_buffer_tx}}
+    l_u8 frame_data_length[] = {
+        5,
+        5
     };
-    l_u8 table_size = sizeof(master_frame_table) / sizeof(master_frame_table[0]);
 
 #ifdef LIN_MASTER
+    uint8_t master_data_buffer[][8] = {
+        {'h', 'e', 'l', 'l', 'o'},
+        {0, 0, 0, 0, 0}
+    };
+    t_master_frame_table_item master_frame_table[] = {
+            {10, 0, {0x02, OPEN_LIN_FRAME_TYPE_TRANSMIT, frame_data_length[0], master_data_buffer[0]}},
+            {10, 0, {0x03, OPEN_LIN_FRAME_TYPE_RECEIVE,  frame_data_length[1], master_data_buffer[1]}}
+    };
+    l_u8 table_size = sizeof(master_frame_table) / sizeof(master_frame_table[0]);
     open_lin_master_dl_init(master_frame_table, table_size);
     open_lin_master_dl_set_state_callback(open_lin_master_state_callback);
     
     unsigned long micro_prev = micros();
     while (1) {
         unsigned long micro_now = micros();
+
+        if (master_state == OPEN_LIN_MASTER_DATA_RX) {
+            uint8_t buf[8];
+            while (swLin.available() <= 0)
+                ;
+            int bytes_read = swLin.read(buf, sizeof(buf));
+
+            for (int i = 0; i < bytes_read; ++i) {
+                open_lin_master_dl_rx(buf[i]);
+            }
+        }
+
         open_lin_master_dl_handler((micro_now - micro_prev)/1000);
         micro_prev = micro_now;
         delay(1);
@@ -74,8 +98,14 @@ extern "C" void app_main()
 #endif
 
 #ifdef LIN_SLAVE
-    uint8_t slave_data_buffer_rx[5];
-    open_lin_frame_slot_t frame_slot[1] = {0x02,OPEN_LIN_FRAME_TYPE_RECEIVE,sizeof(slave_data_buffer_rx), slave_data_buffer_rx};
+    uint8_t slave_data_buffer[][8] = {
+        {0, 0, 0, 0, 0},
+        {'S', 'L', 'A', 'V', 'E'}
+    };
+    open_lin_frame_slot_t frame_slot[] = {
+        {0x02, OPEN_LIN_FRAME_TYPE_RECEIVE,  frame_data_length[0], slave_data_buffer[0]},
+        {0x03, OPEN_LIN_FRAME_TYPE_TRANSMIT, frame_data_length[1], slave_data_buffer[1]}
+    };
     l_u8 slot_size = sizeof(frame_slot) / sizeof(frame_slot[0]);
     open_lin_net_init(frame_slot, slot_size);
 
@@ -84,17 +114,6 @@ extern "C" void app_main()
 
     while (1) {
         if (swLin.checkBreak()) {
-            uint8_t buf[3 + sizeof(master_data_buffer_tx)/sizeof(master_data_buffer_tx[0])];
-            int bytes_to_read = sizeof(buf);
-            int bytes_read = 0;
-            while (bytes_read < bytes_to_read) {
-                while (swLin.available() <= 0)
-                    ;
-                bytes_read += swLin.read(buf+bytes_read, bytes_to_read - bytes_read);
-            }
-
-            open_lin_slave_reset();
-
             // In `open_lin_slave_rx_header` it checks for break,
             // but the swLin.checkBreak() has done the check.
             // Therefore `open_lin_hw_break_reg` is reg to notify
@@ -102,11 +121,16 @@ extern "C" void app_main()
             open_lin_hw_break_reg = true;
             open_lin_slave_rx_header(0); // To notify that the break has detected
 
-            for (int i = 0; i < sizeof(buf)/sizeof(buf[0]); ++i) {
-                open_lin_slave_rx_header(buf[i]);
-            }
+            uint8_t buf[3 + 8];
+            while (slave_state != OPEN_LIN_SLAVE_IDLE) {
+                while (swLin.available() <= 0)
+                    ;
+                int bytes_read = swLin.read(buf, sizeof(buf));
 
-            swLin.endFrame();
+                for (int i = 0; i < bytes_read; ++i) {
+                    open_lin_slave_rx_header(buf[i]);
+                }
+            }
 
             delay(10);
         }
--- src/open_lin_hw_esp32.cpp
+++ src/open_lin_hw_esp32.cpp
@@ -1,3 +1,7 @@
+#ifdef DEBUG
+#include <stdio.h>
+#endif
+
 #include "ESP32-SoftwareLIN/src/SoftwareLin.h"
 extern SoftwareLin swLin;
 
@@ -8,9 +12,6 @@ extern "C" {
 #include "open_lin_types.h"
 #include "open_lin_transport_layer.h"
 
-#ifdef OPEN_LIN_TRANSPORT_LAYER
-open_lin_NAD_t open_lin_NAD;
-#endif
 l_u16 open_lin_function_id;
 l_u16 open_lin_supplier_id;
 
@@ -47,18 +48,26 @@ l_bool open_lin_hw_tx_data(l_u8* data, l_u8 len)
     return swLin.write((const uint8_t*)data, len);
 }
 
-#ifdef OPEN_LIN_DYNAMIC_IDS
-open_lin_id_translation_item_t open_lin_id_translation_tab[1];
-#endif
-
 void open_lin_master_dl_rx_callback(open_lin_frame_slot_t* slot)
 {
-
+#ifdef DEBUG
+    printf("[open_lin_master_dl_rx_callback]\n\t");
+    for (int i = 0; i < slot->data_length; ++i) {
+        printf("0x%02X ", slot->data_ptr[i]);
+    }
+    printf("\n\n");
+#endif
 }
 
 void open_lin_on_rx_frame(open_lin_frame_slot_t *slot)
 {
-
+#ifdef DEBUG
+    printf("[open_lin_on_rx_frame]\n\t");
+    for (int i = 0; i < slot->data_length; ++i) {
+        printf("0x%02X ", slot->data_ptr[i]);
+    }
+    printf("\n\n");
+#endif
 }
 
 void open_lin_set_rx_enabled(l_bool status)
@@ -66,10 +75,16 @@ void open_lin_set_rx_enabled(l_bool status)
 
 }
 
+#ifdef OPEN_LIN_TRANSPORT_LAYER
+open_lin_NAD_t open_lin_NAD;
 void open_lin_sid_callback(open_lin_frame_slot_t* slot)
 {
 
 }
+#endif
+#ifdef OPEN_LIN_DYNAMIC_IDS
+open_lin_id_translation_item_t open_lin_id_translation_tab[1];
+#endif
 
 #ifdef __cplusplus
 }
